255251583a1b2406b02518b298004b4dee465bad
SThevenot <sktmh007@gmail.com>
2023-02-06 13:08:48 -0600

    created connection file and index.js

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+/** @format */
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/my-recipes',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
+
